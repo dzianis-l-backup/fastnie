e04568d6c90e641b1b996904dae0f839535c6b9f
Owrlean .D <leadzian213@gmail.com>
2022-04-05 13:50:14 +0300

    fix: webpack env

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
-module.exports = () => {
+module.exports = (env) => {
     return {
         entry: {
             index: './src/index.tsx',
